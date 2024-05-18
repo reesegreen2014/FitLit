24c0c43ef36c2ad42a118030eb5663304c085075
“Kyle <kylemboomer@gmail.com>
2024-05-18 10:57:10 -0500

    Refactor Hydration Functions

--- src/hydrationData.js
+++ src/hydrationData.js
@@ -2,11 +2,11 @@ import hydrationData from '../src/data/hydration.js';
 
 function calculateAverageFluidOunces(id, hydrationDataObject) {
     const hydrationData = hydrationDataObject.hydrationData;
-        if (hydrationData.length === 0){
+        if (!hydrationData.length){
             return 0;
         }
     const userHydrationData = hydrationData.filter(data => data.userID === id);
-        if (userHydrationData.length === 0){
+        if (!userHydrationData.length){
             return 0;
         }
     const totalFluidOunces = userHydrationData.reduce((total, data) => total + data.numOunces, 0);
@@ -20,7 +20,7 @@ console.log(`Average fluid ounces for yourself:`, calculateAverageFluidOunces(1,
 function calculateDailyFluidOunces(userID, date, hydrationDataObject) {
     const hydrationData = hydrationDataObject.hydrationData;
     const userHydrationData = hydrationData.filter(data => data.userID === userID && data.date === date);
-    if (userHydrationData.length === 0) {
+    if (!userHydrationData.length) {
         return "No data found for the specified user and date.";
     }
     const totalFluidOunces = userHydrationData.reduce((total, data) => total + data.numOunces, 0);
---
+++ src/sleep.js
@@ -0,0 +1,5 @@
+// I need functions that return : 
+// avg hrs sleep per day, avg quality of sleep per day, 
+// access to sleep for a specific day, sleep quality for specific day, 
+// hrs of sleep per week, any week
+// sleep quality for a given week
