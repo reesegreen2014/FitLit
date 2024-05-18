5758cfc08c0bd8ee9596cce95c5c904fb1ff9f32
“Kyle <kylemboomer@gmail.com>
2024-05-18 14:25:57 -0500

    Pass Tests for getDailyHrs function

--- src/sleep.js
+++ src/sleep.js
@@ -5,25 +5,41 @@
 // sleep quality for a given week
 
 function getAverageHrs(userData) {
-    if(!userData.sleepData.length) {
+    if (!userData.sleepData.length) {
         return `No data available.`
     } else {
-    const allSleepHrs = userData.sleepData.map((day) => day.hoursOfSleep)
-    const sleepAvg = allSleepHrs.reduce((total, hours) => total + hours, 0)
-    return sleepAvg/userData.sleepData.length
+        const allSleepHrs = userData.sleepData.map((day) => day.hoursOfSleep)
+        const sleepAvg = allSleepHrs.reduce((total, hours) => total + hours, 0)
+        return sleepAvg / userData.sleepData.length
     }
 }
 
 function getAverageQuality(userData) {
-    if(!userData.sleepData.length) {
+    if (!userData.sleepData.length) {
         return `No data available.`
     } else {
         const allSleepQuality = userData.sleepData.map((day) => day.qualityOfSleep)
         const qualityAvg = allSleepQuality.reduce((total, hours) => total + hours, 0)
-        return qualityAvg/userData.sleepData.length
+        return qualityAvg / userData.sleepData.length
     }
 }
-export {getAverageHrs, 
-    getAverageQuality, 
-    // getDailyHrs, getDailyQuality
+
+function getDailyHrs(userData, targetDate) {
+    if (!userData.sleepData.length) {
+        return `No data available.`
+    }
+    const specifiedSleep = userData.sleepData.find((day) => day.date === targetDate)
+    if (specifiedSleep) {
+        return `You slept ${specifiedSleep.hoursOfSleep} hours on ${specifiedSleep.date}.`
+    } else {
+        return `No data available for that date.`
+    }
+}
+
+
+export {
+    getAverageHrs,
+    getAverageQuality,
+    getDailyHrs,
+    //  getDailyQuality
 }
