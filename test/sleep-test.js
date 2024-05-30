831ab5d2b149ad5a81f3f819e2d0fe312126485a
“Kyle <kylemboomer@gmail.com>
2024-05-29 20:04:41 -0500

    Preserve Refactored Sleep Test

--- test/sleep-test.js
+++ test/sleep-test.js
@@ -53,33 +53,6 @@ before(function () {
 });
 
 describe('Get Average Hours Function', function () {
-<<<<<<< HEAD
-=======
-  let sleepData;
-  beforeEach(function () {
-    sleepData = [
-      {
-        "userID": 1,
-        "date": "2023/03/24",
-        "hoursSlept": 8,
-        "sleepQuality": 4.7
-      },
-      {
-        "userID": 1,
-        "date": "2023/03/25",
-        "hoursSlept": 7,
-        "sleepQuality": 3.8
-      },
-      {
-        "userID": 1,
-        "date": "2023/03/26",
-        "hoursSlept": 6,
-        "sleepQuality": 2.1
-      }
-    ];
-  });
-
->>>>>>> ce9437e22283e392d5832ebf17ae4f67aecc1ce9
   it('should calculate average hours of sleep for all time', () => {
     const sleepAvg = getAverageHrs(sleepData, 1);
     expect(sleepAvg).to.equal('7.00');
@@ -105,33 +78,6 @@ describe('Get Average Hours Function', function () {
 });
 
 describe('Get Sleep Quality Average', function () {
-<<<<<<< HEAD
-=======
-  let sleepData;
-  beforeEach(function () {
-    sleepData = [
-      {
-        "userID": 1,
-        "date": "2023/03/24",
-        "hoursSlept": 8,
-        "sleepQuality": 4.7
-      },
-      {
-        "userID": 1,
-        "date": "2023/03/25",
-        "hoursSlept": 7,
-        "sleepQuality": 3.8
-      },
-      {
-        "userID": 1,
-        "date": "2023/03/26",
-        "hoursSlept": 6,
-        "sleepQuality": 2.1
-      }
-    ];
-  });
-
->>>>>>> ce9437e22283e392d5832ebf17ae4f67aecc1ce9
   it('should calculate average quality of sleep for all time', () => {
     const sleepAvg = getAverageQuality(sleepData, 1);
     expect(sleepAvg).to.equal('4.89');
@@ -157,33 +103,6 @@ describe('Get Sleep Quality Average', function () {
 });
 
 describe('Get Daily Sleep Data', function () {
-<<<<<<< HEAD
-=======
-  let sleepData;
-  beforeEach(function () {
-    sleepData = [
-      {
-        "userID": 1,
-        "date": "2023/03/24",
-        "hoursSlept": 8,
-        "sleepQuality": 4.7
-      },
-      {
-        "userID": 1,
-        "date": "2023/03/25",
-        "hoursSlept": 7,
-        "sleepQuality": 3.8
-      },
-      {
-        "userID": 1,
-        "date": "2023/03/26",
-        "hoursSlept": 6,
-        "sleepQuality": 2.1
-      }
-    ];
-  });
-
->>>>>>> ce9437e22283e392d5832ebf17ae4f67aecc1ce9
   it('should return sleep data for a specified day', () => {
     const specifiedSleep = getDailyHrs(sleepData, 1, '2023/03/24');
     expect(specifiedSleep).to.equal('You slept 7 hours on 2023/03/24.');
@@ -196,33 +115,6 @@ describe('Get Daily Sleep Data', function () {
 });
 
 describe('Get Daily Sleep Quality', function () {
-<<<<<<< HEAD
-=======
-  let sleepData;
-  beforeEach(function () {
-    sleepData = [
-      {
-        "userID": 1,
-        "date": "2023/03/24",
-        "hoursSlept": 8,
-        "sleepQuality": 4.7
-      },
-      {
-        "userID": 1,
-        "date": "2023/03/25",
-        "hoursSlept": 7,
-        "sleepQuality": 3.8
-      },
-      {
-        "userID": 1,
-        "date": "2023/03/26",
-        "hoursSlept": 6,
-        "sleepQuality": 2.1
-      }
-    ];
-  });
-
->>>>>>> ce9437e22283e392d5832ebf17ae4f67aecc1ce9
   it('should return sleep quality for a specified day', () => {
     const specifiedQuality = getDailyQuality(sleepData, 1, '2023/03/24');
     expect(specifiedQuality).to.equal('You experienced a sleep quality of 4.7 on 2023/03/24.');
@@ -234,7 +126,6 @@ describe('Get Daily Sleep Quality', function () {
   });
 });
 
-<<<<<<< HEAD
 describe('Get Most Recent Sleep Data', function() {
   it('should return most recent week of sleep data', () => {
     const weekOfSleep = getRecentSleep(sleepData)
@@ -290,5 +181,3 @@ describe('Get Most Recent Sleep Data', function() {
     expect(lessThanWeekOfSleep).to.deep.equal(expectedSleepData)
   })
 })
-=======
->>>>>>> ce9437e22283e392d5832ebf17ae4f67aecc1ce9
